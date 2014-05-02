7af1fbb4071afecf5bf99995c89c1630426949cf
jlucidar <jlucidar@gmail.com>
2014-05-02 09:12:16 -0400

    reorganization of the folders

---
+++ SHOPBOT_DETECTION/client/README.md
@@ -0,0 +1,10 @@
+SHOPBOT AUTO DETECTION SOFTWARE v1.0
+====================================
+
+this is the client software for auto-detect a shopbot tools on a local network. 
+
+the program return a ip adresses list of connected tools.
+
+the current version doesn't allow you to run it into Windows (not tested on MAC).
+
+ ===================================
--- SHOPBOT_DETECTION/client/linux/detect.sh
+++ SHOPBOT_DETECTION/client/linux/detect.sh
@@ -10,7 +10,6 @@
 #-----------------------------------------#
 
 execPath=$(dirname $0)
-echo $execPath
 
 
 list_itf=$(ifconfig | awk -F "  " '{ print $1 }' | tr -s "\n") #get the list of the interfaces
@@ -28,7 +27,7 @@ do
 		for i in `seq 0 1 254`;
 		do
 			
-			eval 'ping $ip_netw.$i -c 1  -W 1 >/dev/null ; if [ $? -eq 0 ] ;then echo 192.168.33.$i; fi >> $execPath/ip.dat' &
+			eval 'ping $ip_netw.$i -c 1  -W 1 >/dev/null ; if [ $? -eq 0 ] ;then echo 192.168.33.$i; fi >> "$execPath/ip.dat"' &
 		done
 	else
 		is_B=$(echo $ip_mask | grep 255.255. | wc -l)
@@ -39,7 +38,7 @@ do
 			do
 				for j in `seq 0 1 254`;
 				do
-					eval 'ping $ip_netw.$i.$j -c 1  -W 1 >/dev/null ; if [ $? -eq 0 ] ;then echo 192.168.$i.$j; fi >> $execPath/ip.dat' &
+					eval 'ping $ip_netw.$i.$j -c 1  -W 1 >/dev/null ; if [ $? -eq 0 ] ;then echo 192.168.$i.$j; fi >> "$execPath/ip.dat"' &
 				done
 			done
 		fi
@@ -50,10 +49,10 @@ done
 wait # wait for every processus to end
 
 
-cat $execPath/ip.dat | sort | uniq >$execPath/ip2.dat; cp $execPath/ip2.dat $execPath/ip.dat; rm $execPath/ip2.dat;
+cat "$execPath/ip.dat" | sort | uniq >"$execPath/ip2.dat"; cp "$execPath/ip2.dat" "$execPath/ip.dat"; rm "$execPath/ip2.dat";
 
 for line in $(cat $execPath/ip.dat);
 	do eval './are_you_a_sbt "$line" 2> /dev/null ; if [ $? -eq 0 ]; then echo $line; fi'&
 done
 wait
-rm $execPath/ip.dat 2> /dev/null
+rm "$execPath/ip.dat" 2> /dev/null

979e16fdaabe19f336c3829724451222a28c5b8d
jlucidar <jlucidar@gmail.com>
2014-05-07 00:56:39 -0400

    correction of the ip attribution (add quotes) redirection on port 22 ( ssh ) adn not 80
    /!\ I shall change the use of a global variable for a data file !
    ( deleting problem of the Upnpc rules )
    
    Signed-off-by: jlucidar <jlucidar@gmail.com>

--- SHOPBOT_DISTANT_CONNECTION/server/upnpc_conf.sh
+++ SHOPBOT_DISTANT_CONNECTION/server/upnpc_conf.sh
@@ -13,17 +13,20 @@ upnpc -d $UPNPC_PUBLIC_PORT TCP > /dev/null 2>&1
 export UPNPC_PUBLIC_PORT=""
 
 #add a port forwarding : from 77XX to $ip:80
-ip= upnpc -l | grep "Local LAN ip address" | cut -d: -f2
-echo $ip
+ip=` upnpc -l | grep "Local LAN ip address" | cut -d: -f2`
+
 public_port=$(( 7700+( $(od -An -N2 -i /dev/random) )%(100) ))
 
-upnpc -a $ip 80 $public_port TCP > /dev/null 2>&1
+
+upnpc -a $ip 22 $public_port TCP >/dev/null 2>&1
 
 if [ $? -eq 0 ]
 then
-	export UPNPC_PUBLIC_PORT=$public_port
+	UPNPC_PUBLIC_PORT=$public_port
+	export UPNPC_PUBLIC_PORT
 	echo "port forwarding succcessful on port $UPNPC_PUBLIC_PORT"
 else	
+	echo $?
 	echo "no upnp router available..."
 fi
 

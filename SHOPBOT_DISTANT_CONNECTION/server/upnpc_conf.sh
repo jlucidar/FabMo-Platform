d3f90551ac03797c2b264d186705ad8b1de1f142
jlucidar <jlucidar@gmail.com>
2014-05-08 10:58:46 -0400

    Open only one port on the router. save the number in UPNPC_PUBLIC_PORT.dat. verify that the port is correctly configure.
    Signed-off-by: jlucidar <jlucidar@gmail.com>

--- SHOPBOT_DISTANT_CONNECTION/server/upnpc_conf.sh
+++ SHOPBOT_DISTANT_CONNECTION/server/upnpc_conf.sh
@@ -7,26 +7,29 @@
 # variable $UPNPC_PUBLIC_PORT.		      #
 ###############################################
 
+execPath=$(dirname $0)
+
 
 #remove last port forwarding : from 77XX
+UPNPC_PUBLIC_PORT=`cat  $execPath/UPNPC_PUBLIC_PORT.dat`
 upnpc -d $UPNPC_PUBLIC_PORT TCP > /dev/null 2>&1
-export UPNPC_PUBLIC_PORT=""
+rm $execPath/UPNPC_PUBLIC_PORT.dat > /dev/null 2>&1
 
 #add a port forwarding : from 77XX to $ip:80
-ip=` upnpc -l | grep "Local LAN ip address" | cut -d: -f2`
+ip=`upnpc -l | grep "Local LAN ip address" | cut -d" " -f6`
 
 public_port=$(( 7700+( $(od -An -N2 -i /dev/random) )%(100) ))
 
 
-upnpc -a $ip 22 $public_port TCP >/dev/null 2>&1
+`upnpc -a $ip 22 $public_port TCP >/dev/null 2>&1`
 
-if [ $? -eq 0 ]
+$is_success=`upnpc -l | grep "TCP  $public_port->$ip:22" | wc -l >/dev/null 2>&1`
+if [ $is_success -eq 1 ]
 then
 	UPNPC_PUBLIC_PORT=$public_port
-	export UPNPC_PUBLIC_PORT
 	echo "port forwarding succcessful on port $UPNPC_PUBLIC_PORT"
+	echo $UPNPC_PUBLIC_PORT > $execPath/UPNPC_PUBLIC_PORT.dat
 else	
-	echo $?
 	echo "no upnp router available..."
 fi
 

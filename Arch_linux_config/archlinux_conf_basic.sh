afeca58823dae719161cc3a95010da620a7e9072
jlucidar <jlucidar@gmail.com>
2014-05-08 13:51:50 -0400

    add the support for ethernet over usb enable dhcp server on ethernet over usb enable these services at the startup
    Signed-off-by: jlucidar <jlucidar@gmail.com>

--- Arch_linux_config/archlinux_conf_basic.sh
+++ Arch_linux_config/archlinux_conf_basic.sh
@@ -27,6 +27,9 @@ pacman -S miniupnpc --noconfirm
 # install gcc
 pacman -S gcc --noconfirm
 
+# install the dhcp server
+pacman -S dhcp --noconfirm
+
 # update the system
 pacman -Syu --noconfirm
 
@@ -42,6 +45,24 @@ pacman -Syu --noconfirm
 # launch at the start-up the following : wpa_supplicant, wifi auto manager
 systemctl enable /lib/systemd/system/wpa_supplicant.service
 systemctl enable netctl-auto@wlp2s0.service
+
+# allow ethernet over usb at the startup 
+echo "g_ether" >  /etc/modules-load.d/g_ether.conf
+# mount the usb0 interface if it is available, and configure it with the ip 192.168.10.1, netmask 255.255.255.252
+echo -e "Description='A basic dhcp ethernet over usb connection'\nInterface=usb0\nConnection=ethernet\nIP=static\nAddress=('192.168.10.1/29')" > /etc/netctl/usb0
+#enable at startup
+netctl enable usb0
+
+#dhcp service on usb0
+
+# dhcpd.conf\n
+# usb0 dhcp service\n
+echo -e "subnet 192.168.10.0 netmask 255.255.255.248 {\n  range 192.168.10.2 192.168.10.6;\n  default-lease-time 600;\n  max-lease-time 7200;\n}\n" > /etc/dhcpd.conf
+
+echo -e "[Unit]\nDescription=IPv4 DHCP server on usb0\nWants=network.target\nAfter=network.target\n\n[Service]\nType=forking\nPIDFile=/run/dhcpd4.pid\nExecStart=/usr/bin/dhcpd -4 -q -pf /run/dhcpd4.pid usb0\nKillSignal=SIGINT\n\n[Install]\nWantedBy=multi-user.target\n" > /etc/systemd/system/dhcpd_usb0.service
+systemctl enable dhcpd_usb0
+
+
 
 
 echo "end of the configuration ! reboot . . ."

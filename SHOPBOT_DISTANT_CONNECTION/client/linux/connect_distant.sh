7e32fee8044f669db8b0c90e1216bba0da64c1b1
jlucidar <jlucidar@gmail.com>
2014-05-08 11:25:04 -0400

    Add single ssh tunnel connexion use user@ip instead of ip save the PID of the ssh tunnel in SH_TUNNEL_PID.dat
    Signed-off-by: jlucidar <jlucidar@gmail.com>

--- SHOPBOT_DISTANT_CONNECTION/client/linux/connect_distant.sh
+++ SHOPBOT_DISTANT_CONNECTION/client/linux/connect_distant.sh
@@ -11,6 +11,9 @@
 # Return the local port of the tunnel.        #
 ###############################################
 
+execPath=$(dirname $0)
+
+
 if [ "$1" = "" ]
 then 
 echo "syntax : connect_distant.sh ip_adress port"
@@ -29,14 +32,24 @@ echo "syntax : connect_distant.sh ip_adress port"
 exit
 fi
 
+# delete the former ssh_tunnel if there is one
+kill `cat $execPath/SSH_TUNNEL_PID.dat` >/dev/null 2>&1
+rm $execPath/SSH_TUNNEL_PID.dat >/dev/null 2>&1
+
+
+
 local_port=$(( 5500+( $(od -An -N2 -i /dev/random) )%(100) ))
-distant_public_ip=$1
+user_at_distant_public_ip=$1
 distant_public_port=$2
 
 # set the ssh tunnel for accessing the web server on the raspberry pi from everywhere
 
-ssh -f -N -L :$local_port:localhost:80 handibot-dev@$distant_public_ip -p $distant_public_port
+ssh -f -N -L :$local_port:localhost:80 $user_at_distant_public_ip -p $distant_public_port
 if [ $? -eq 0 ]
 then
-echo $local_port 
+	ps aux | grep "[s]sh -f -N -L :$local_port:localhost:80 $user_at_distant_public_ip -p $distant_public_port" | awk -F " " '{ print $2 }' > $execPath/SSH_TUNNEL_PID.dat
+	echo "SSH tunnel opened on localhost:$local_port"
+else
+	echo "ssh error : cannot open the tunnel"
+
 fi

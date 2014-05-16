24e018ae04a0329db906881d310b6d2b7e723be9
jlucidar <jlucidar@gmail.com>
2014-05-16 16:13:48 -0400

    add support for wlan1 interface (dedicated to WIFI DIRECT)
    Signed-off-by: jlucidar <jlucidar@gmail.com>

--- Arch_linux_config/script_wifi_p2p.sh
+++ Arch_linux_config/script_wifi_p2p.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 # require udhcp working for android
-wpa_cli p2p_find
-wpa_cli p2p_group_add
-wps_pin any 12345678
+wpa_cli -i wlan1 p2p_find
+wpa_cli -i wlan1 p2p_group_add
+wpa_cli -i wlan1 wps_pin any 12345678
